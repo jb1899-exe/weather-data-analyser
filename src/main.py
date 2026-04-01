90ebf4ff6499cc38510f539934604a6097d3167f
buchananja <buchanan.j.a@proton.me>
2026-04-01 21:33:37 +0100

    Add user interface for selecting analytical operations.

--- src/main.py
+++ src/main.py
@@ -4,8 +4,12 @@ def get_head(arr, n):
     return arr[:n]
 
 
-def get_filename():
-    pass
+def print_filenames(filepaths: list):
+    '''Prints availible filenames in filepaths'''
+    
+    print('Availible files: ')
+    for filename in filepaths:
+        print(f'    `{filename}`')
 
 
 def get_record_count(data: list):
@@ -17,9 +21,10 @@ def get_record_count(data: list):
     return count
 
 
-def load_data(filepath: str) -> list:
+def load_data(datapath: str, filename: str) -> list:
     '''Takes string filepath and returns list of data.'''
 
+    filepath = f'{datapath}{filename}'
     data = list()
     with open(filepath, 'r') as file:
         for i, row in enumerate(file):
@@ -28,32 +33,53 @@ def load_data(filepath: str) -> list:
             except ValueError:
                 print(f'ERROR: Non-numeric found at record: {i}!')
                 continue
+    city_name = filename[:-8]
+    print(f'    {city_name} [{get_record_count(data)} records]: {get_head(data, 10)}...')
     return data
 
 
 def main() -> None:
 
-    # allow user to select wich data to analyse?
     filepaths = [
         'CityA_365.txt',
         'CityB_365.txt',
-        'CityC_365.txt'
+        'CityC_365.txt',
+        'CityA_1460.txt',
+        'CityB_1460.txt',
+        'CityC_1460.txt',
+        'CityN_100000.txt'
     ]
     datapath = 'data/'
 
-    data_store = dict()
-    print('loading data...')
-    for filepath in filepaths:
-        city_name = filepath[:-8]
-        data = load_data(f'{datapath}{filepath}')
-        data_store[city_name] = data
-        print(f'    {city_name} [{get_record_count(data)} records]: {get_head(data, 10)}...')
-
-
-
-
+    print_filenames(filepaths)
     while True:
-        operation_input = input()
+        filename_input = input('\nPlease enter a filename to load (enter "quit" to exit): ')
+        if filename_input in filepaths:
+            data = load_data(datapath, filename_input)
+            
+            while True:
+                operation_input = input('\nPlease enter an operation (search, sort, merge): ').lower()
+                if operation_input == 'search':
+                    print('Searching...')
+                    break
+                
+                elif operation_input == 'sort':
+                    print('Sorting...')
+                    break
+                
+                elif operation_input == 'merge':
+                    print('Merging...')
+                    break
+                
+                else:
+                    print('Please enter a valid input!\n')
+        
+        elif filename_input.lower() in ['q', 'quit']:
+            print('Quitting...\n')
+            break
+        
+        else:
+            print('please enter a valid input!\n')
 
 
 if __name__ == '__main__':
